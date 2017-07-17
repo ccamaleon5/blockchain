69d4436603a925d8333c47535db5c2cc2bd190bf
ccamaleon5 <adriancc5.5@gmail.com>
2017-07-17 15:49:03 -0500

    Update vivanda.go

--- vivanda.go
+++ vivanda.go
@@ -9,6 +9,7 @@ import (
 	"errors"
 	"fmt"
 	"strconv"
+	"time"
 
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 	"github.com/hyperledger/fabric/core/util"
@@ -18,7 +19,16 @@ import (
 )
 
 const business string = "Vivanda"
-const walletContract string = "cf359028b7eb7e6b8f12f30803b507909e843c1e247d142d53e9e51bca528460baad83b83ad84cd5087eda3f2fca1e647437913f14c09a676d38dbe367bea361"
+const walletContract string = "b2a6d8acbb81e442555d94daa803ba0130218cfacf70d2bbd94b75349558bac10b70289b40f665dfc83adfab56fbf060edaa72b9f9c77abfb950dff30e003cae"
+const change float64 = 2
+
+const (
+	tableColumn     = "CanjesVivanda"
+	columnTime      = "Time"
+	columnAccountID = "Account"
+	columnAmount    = "Amount"
+	columnType      = "Type"
+)
 
 // UUID layout variants.
 const (
@@ -37,18 +47,34 @@ type UUID [16]byte
 
 //Wallet - Structure for products used in buy goods
 type Wallet struct {
-	Id        string    `json:"id"`
-	Email     string  `json:"email"`
-	Phone     string  `json:"phone"`
-	Document  string `json:"document"`
-	Password  string `json:"password"` 
-	Amount    float64 `json:"amount"` 
+	Id       string  `json:"id"`
+	Email    string  `json:"email"`
+	Phone    string  `json:"phone"`
+	Document string  `json:"document"`
+	Password string  `json:"password"`
+	Amount   float64 `json:"amount"`
+}
+
+//Movimiento - Structure for movements
+type Movement struct {
+	Time     int64   `json:"time"`
+	WalletId string  `json:"walletid"`
+	Amount   float64 `json:"amount"`
+	Type     string  `json:"type"`
+}
+
+//Balance - Structure for balance
+type Balance struct{
+	Business string `json:"business"`
+	Total float64 `json:"total"`
+	Exchange float64 `json:"exchange"`
+	Send float64 `json:"send"`   
 }
 
 //Response - Structure for response
 type ResponseContract struct {
-	Code        int32    `json:"code"`
-	Response    string  `json:"response"` 
+	Code     int32  `json:"code"`
+	Response string `json:"response"`
 }
 
 // SimpleChaincode example simple Chaincode implementation
@@ -68,11 +94,39 @@ func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string
 	if len(args) != 1 {
 		return nil, errors.New("Número de Argumentos incorrecto. Se esperaba 1 argumento")
 	}
+	
+	amt, err := strconv.ParseFloat(args[0], 64)
 
-	err := stub.PutState("coinBalance", []byte(args[0]))
 	if err != nil {
+		fmt.Println("Error Float parsing")
+		return nil, errors.New("Error marshaling wallet")
+	}
+	
+	balance := Balance{
+		Business: "Vivanda",
+		Total:    amt,
+		Exchange: 0,
+		Send:     0,
+	}
+
+	bytes, err1 := json.Marshal(balance)
+	if err1 != nil {
+		fmt.Println("Error marshaling wallet")
+		return nil, errors.New("Error marshaling wallet")
+	}
+
+	err = stub.PutState("coinBalance", bytes)
+	if err != nil {
+		fmt.Println("Error creando el balance inicial del negocio")
 		return nil, err
 	}
+	
+	stub.CreateTable(tableColumn, []*shim.ColumnDefinition{
+		&shim.ColumnDefinition{Name: columnAccountID, Type: shim.ColumnDefinition_STRING, Key: true},
+		&shim.ColumnDefinition{Name: columnTime, Type: shim.ColumnDefinition_INT64, Key: true},
+		&shim.ColumnDefinition{Name: columnAmount, Type: shim.ColumnDefinition_STRING, Key: false},
+		&shim.ColumnDefinition{Name: columnType, Type: shim.ColumnDefinition_STRING, Key: false},
+	})
 
 	return nil, nil
 }
@@ -98,7 +152,10 @@ func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function strin
 	// Manejar diferentes funciones
 	if function == "getbalance" {
 		return t.getBalance(stub, args)
+	} else if function == "gettotalcoin" {
+			return t.getTotalCoin(stub, args)
 	}
+	
 	fmt.Println("query no encuentra la funcion: " + function)
 
 	return nil, errors.New("Funcion invocada desconocida: " + function)
@@ -107,7 +164,7 @@ func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function strin
 // createWallet - invocar esta funcion para crear un wallet con saldo inicial
 func (t *SimpleChaincode) createWallet(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
 	fmt.Println("Vivanda Call---Funcion createWallet---")
-	
+
 	if len(args) != 5 {
 		return nil, errors.New("Numero incorrecto de argumentos.Se espera 5 para createWallet")
 	}
@@ -129,25 +186,23 @@ func (t *SimpleChaincode) createWallet(stub shim.ChaincodeStubInterface, args []
 // createWallet - invocar esta funcion para crear un wallet con saldo inicial
 func (t *SimpleChaincode) buy(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
 	fmt.Println("Vivanda Call---Funcion Buy---")
-	
+
 	if len(args) != 3 {
 		return nil, errors.New("Numero incorrecto de argumentos.Se espera 3 para buy")
 	}
 
-	var change float64 = 2
-	
 	solesTotal, err := strconv.ParseFloat(args[1], 64)
 	if err != nil {
 		errStr := fmt.Sprintf("Fallo convertir cadena a float: %s", err.Error())
 		return nil, errors.New(errStr)
-	} 
-	
+	}
+
 	coins, err1 := strconv.ParseFloat(args[2], 64)
 	if err1 != nil {
 		errStr := fmt.Sprintf("Fallo convertir cadena a float: %s", err.Error())
 		return nil, errors.New(errStr)
 	}
-	
+
 	f := "getbalance"
 	queryArgs := util.ToChaincodeArgs(f, args[0])
 	responseQuery, err2 := stub.QueryChaincode(walletContract, queryArgs)
@@ -160,41 +215,55 @@ func (t *SimpleChaincode) buy(stub shim.ChaincodeStubInterface, args []string) (
 
 	responseContract := ResponseContract{}
 	err1 = json.Unmarshal(responseQuery, &responseContract)
-	
+
 	f = "debitbalance"
-	
-	solesSubtotal := (solesTotal * change) - coins  //Cambiando a Coins
-	
+
+	solesSubtotal := (solesTotal * change) - coins //Cambiando a Coins
+
 	//Compra soles subtotal y canje coins
 	if solesSubtotal > 0 && coins > 0 {
-		
-		coinBalance,_ := strconv.ParseFloat(responseContract.Response, 64)
-		
-		if  coinBalance <= coins {
-			return nil,errors.New("El cliente no cuenta con coins suficientes")
+
+		coinBalance, _ := strconv.ParseFloat(responseContract.Response, 64)
+
+		if coinBalance <= coins {
+			return nil, errors.New("El cliente no cuenta con coins suficientes")
 		}
-			
-		coins = solesSubtotal //- coins 
+
+		//Debitar Coins Usuario
+		invokeArgs2 := util.ToChaincodeArgs(f, args[0], business, strconv.FormatFloat(coins, 'f', 6, 64))
+		response2, err6 := stub.InvokeChaincode(walletContract, invokeArgs2)
+		if err6 != nil {
+			errStr := fmt.Sprintf("Failed to invoke chaincode. Got error: %s", err6.Error())
+			fmt.Printf(errStr)
+			return nil, errors.New(errStr)
+		}
+
+		fmt.Printf("Invoke chaincode successful. Got response %s", string(response2))
+		
+		insertRow(stub,strconv.FormatFloat(coins, 'f', 6, 64),"C")
+
+		//Cargar Coins Usuario
+		coins = solesSubtotal //- coins
 		if coins < 0 {
-			coins = coins*-1
+			coins = coins * -1
 			f = "debitbalance"
 		} else {
 			f = "putbalance"
 		}
 	} else {
-		if solesSubtotal > 0 {  //Compra Soles
-			f = "putbalance"					
+		if solesSubtotal > 0 { //Compra Soles
+			f = "putbalance"
 			coins = solesTotal * change
-		}else if coins > 0 {  //Canje Coins
-			coinBalance,_ := strconv.ParseFloat(responseContract.Response, 64)
+		} else if coins > 0 { //Canje Coins
+			coinBalance, _ := strconv.ParseFloat(responseContract.Response, 64)
 			if coinBalance <= coins {
-				return nil,errors.New("El cliente no cuenta con coins suficientes")
+				return nil, errors.New("El cliente no cuenta con coins suficientes")
 			}
 			f = "debitbalance"
 		}
 	}
 
-	invokeArgs := util.ToChaincodeArgs(f, args[0], business, strconv.FormatFloat(coins,'f',6,64))
+	invokeArgs := util.ToChaincodeArgs(f, args[0], business, strconv.FormatFloat(coins, 'f', 6, 64))
 	response, err4 := stub.InvokeChaincode(walletContract, invokeArgs)
 	if err4 != nil {
 		errStr := fmt.Sprintf("Failed to invoke chaincode. Got error: %s", err4.Error())
@@ -203,13 +272,24 @@ func (t *SimpleChaincode) buy(stub shim.ChaincodeStubInterface, args []string) (
 	}
 
 	fmt.Printf("Invoke chaincode successful. Got response %s", string(response))
+	
+	if f == "putbalance" {
+		insertRow(stub,strconv.FormatFloat(coins, 'f', 6, 64),"D")
+	} else{
+		insertRow(stub,strconv.FormatFloat(coins, 'f', 6, 64),"C")
+	}
+	
+	if false == updateBalance(stub, coins, solesSubtotal) {
+		errStr := fmt.Sprintf("Failed update balance")
+		return nil, errors.New(errStr)
+	} 
 
 	return nil, nil
 }
 
 func (t *SimpleChaincode) getBalance(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
 	fmt.Println("Vivanda----getBalance() is running----")
-	
+
 	if len(args) != 1 {
 		return nil, errors.New("Numero incorrecto de argumentos.Se espera 1 para getBalance")
 	}
@@ -227,14 +307,133 @@ func (t *SimpleChaincode) getBalance(stub shim.ChaincodeStubInterface, args []st
 
 	responseContract := ResponseContract{}
 	err1 := json.Unmarshal(response, &responseContract)
-	
+
 	fmt.Println(responseContract.Response)
 	if err1 != nil {
 		fmt.Println("Error parseando a Json" + args[0])
 		return nil, errors.New("Error retrieving Balance" + args[0])
 	}
+
+	return []byte(fmt.Sprintf(`{"code":0,"response":"%s"}`, responseContract.Response)), nil
+}
+
+func (t *SimpleChaincode) getTotalCoin(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	fmt.Println("Call----getTotalCoin() is running----")
+
+	coinBalance, err := stub.GetState("coinBalance")
+	fmt.Println(coinBalance)
+	if err != nil {
+		fmt.Println("Error retrieving coinBalance")
+		return nil, errors.New("Error retrieving coinBalance")
+	}
+
+	return []byte(fmt.Sprintf(`{"code":0,"response":"%s"}`, coinBalance)), nil
+}
+
+//Obtener los movimientos de los coins en Vivanda
+func (t *SimpleChaincode) getMovimientos(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	fmt.Println("Call----getMovimientos() is running----")
+
+	if len(args) != 1 {
+		return nil, errors.New("Incorrecto numero de argumentos. Se esperaba 1")
+	}
+
+	walletId := args[0] // wallet id
+	fmt.Println("Business id is ")
+	fmt.Println(walletId)
+	var columns []shim.Column
+	col1 := shim.Column{Value: &shim.Column_String_{String_: walletId}}
+	columns = append(columns, col1)
+
+	rowChannel, err := stub.GetRows(tableColumn, columns)
+	if err != nil {
+		return nil, fmt.Errorf("getRowTableOne operation failed. %s", err)
+	}
+
+	movimientos := []Movement{}
+	for {
+		select {
+		case row, ok := <-rowChannel:
+			if !ok {
+				rowChannel = nil
+			} else {
+				columnas := row.GetColumns()
+				amountRow, _ := strconv.ParseFloat(columnas[2].GetString_(), 64)
+				movimiento := Movement{Time: columnas[1].GetInt64(), WalletId: columnas[0].GetString_(), Amount: amountRow, Type: columnas[3].GetString_()}
+
+				movimientos = append(movimientos, movimiento)
+			}
+		}
+		if rowChannel == nil {
+			break
+		}
+	}
+
+	jsonRows, err := json.Marshal(movimientos)
+	if err != nil {
+		return nil, fmt.Errorf("getRows Movimientos operation failed. Error marshaling JSON: %s", err)
+	}
+
+	return jsonRows, nil
+}
+
+//Insertar Row de Retorno y Entrega de Coins al Usuario
+func insertRow(stub shim.ChaincodeStubInterface, amount string, tipo string) bool {
 	
-	return []byte(fmt.Sprintf(`{"code":0,"response":"%s"}`,responseContract.Response)), nil
+	//Insertar Row de Retorno de Coins al Negocio
+		a := makeTimestamp()
+
+		fmt.Printf("Time: %d \n", a)
+	
+		var columns []*shim.Column
+		col0 := shim.Column{Value: &shim.Column_String_{String_: business}}
+		col1 := shim.Column{Value: &shim.Column_Int64{Int64: a}}
+		col2 := shim.Column{Value: &shim.Column_String_{String_: amount}}
+		col3 := shim.Column{Value: &shim.Column_String_{String_: tipo}}
+		
+		columns = append(columns, &col0)
+		columns = append(columns, &col1)
+		columns = append(columns, &col2)
+		columns = append(columns, &col3)
+	
+		row := shim.Row{Columns: columns}
+		ok, err := stub.InsertRow(tableColumn, row)
+		if err != nil {
+			return false
+		}
+		if !ok {
+			return false
+		}
+	return true;
+}
+
+//Cambiar el balance de coins del negocio
+func updateBalance(stub shim.ChaincodeStubInterface, coins float64, subtotalSoles float64) bool {
+	bytesWallet1, err1 := stub.GetState("coinBalance")
+
+	balance := Balance{}
+	err := json.Unmarshal(bytesWallet1, &balance)
+
+	fmt.Println(balance)
+	if err1 != nil {
+		fmt.Println("Error retrieving balance")
+		return false
+	}
+
+	//amt, err := strconv.ParseFloat(args[2], 64)
+
+	balance.Exchange = balance.Exchange + coins
+	balance.Send = balance.Send + subtotalSoles 
+	balance.Total = balance.Total - subtotalSoles
+
+	balanceJSONasBytes, _ := json.Marshal(balance)
+	err = stub.PutState("coinBalance", balanceJSONasBytes) //rewrite the wallet
+
+	if err != nil {
+		return false
+	}
+
+	return true
 }
 
 func safeRandom(dest []byte) {
@@ -295,4 +494,8 @@ func NewV4() UUID {
 	u.SetVariant()
 
 	return u
+}
+
+func makeTimestamp() int64 {
+	return time.Now().UnixNano() / int64(time.Millisecond)
 }
